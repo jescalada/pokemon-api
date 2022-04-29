e7610cdd7e3a2e2eb9929027118a96142052a436
Juan Escalada <juanescalada175@gmail.com>
2022-04-29 12:52:29 -0700

    Add load to DOM for name search

--- search.js
+++ search.js
@@ -33,3 +33,47 @@ async function loadPokemonListByAbility(ability) {
     return pokemonList;
 }
 
+function getParams() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return {
+        id: urlParams.get('id'),
+        name: urlParams.get('name'),
+        type: urlParams.get('type'),
+        ability: urlParams.get('ability')
+    }
+}
+
+async function getPokemonBasicData(name) {
+    let pokemon = await loadPokemonByName(name);
+    let result = {
+        id: pokemon['id'],
+        name: pokemon['name'],
+        sprite: pokemon.sprites.other['official-artwork'].front_default
+    };
+    return result;
+}
+
+async function searchByName() {
+    let name = $("#search-box").val().toLowerCase();
+    let pokemon = await getPokemonBasicData(name).then((pokemon) => {
+        let grid = `
+            <div id="grid">
+            `;
+        for (row = 0; row < 1; row++) {
+            grid += `<div class="row">`;
+            for (col = 0; col < 1; col++) {
+                index = 0;
+                grid += `
+            <div class="img-container" onclick="location.href='search.html?id=${pokemon.id}'">
+                <img src="${pokemon.sprite}" alt="${pokemon.name}" style="width:100%">
+            </div> 
+            `;
+            }
+            grid += `</div>`;
+        }
+        grid += `</div>`;
+        $("#results").html(grid);
+    });
+
+}
